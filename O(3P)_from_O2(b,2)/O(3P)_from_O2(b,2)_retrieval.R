b96ea97ed0b84d9929d02db4f114ec5207875eb3
Yankovsky Andrey <yankovskyandrey@gmail.com>
2013-10-20 17:57:29 +0400

    new retrieval formula and results; reorganisation completed

--- O(3P)_from_O2(b,2)/O(3P)_from_O2(b,2)_retrieval.R
+++ O(3P)_from_O2(b,2)/O(3P)_from_O2(b,2)_retrieval.R
@@ -1,16 +1,19 @@
-data <- read.csv('science/data.csv', sep=';', check.names=F)
-constants <- read.csv('science//constants.csv', sep=';', check.names=F)
+data <- read.csv('science/O(3P)_from_O2(b,2)/data.csv', sep=';', check.names=F)
+constants <- read.csv('science/O(3P)_from_O2(b,2)/constants.csv', sep=';', check.names=F)
 
 GetValueWithError <- function(value, error) {
-  value * (1 + rnorm(1, sd=error/2))
+  value * (1 + runif(1, -error, error))
 }
 
-GetConstant <- function(name) {
+C <- function(name) {
   constant <- constants[, name]
-  GetValueWithError(constant[1], constant[1])
+  error = if (is.na(constant[2])) 0 else constant[2]
+  GetValueWithError(constant[1], error)
 }
 
-GetDataValue <- function(i, name) {
+D <- function(i, name) {
+  if (is.null(data[i, name]))
+    stop(paste('no parameter with name', name))
   GetValueWithError(data[i, name], constants[2, 'Rest'])  
 }
 
@@ -20,14 +23,14 @@ for (i in 1:nrow(data)) {
   atomic.oxygen.denceties <- numeric()
   for (j in 1:MAX) {
     new.dencity <- 
-      (1 / GetConstant('K(b2;O)')) * 
+      (1 / C('K(b2;O)')) * 
       (
-        GetDataValue(i, 'J_b2') * GetDataValue(i, 'O2') / data[i, ]$'Nb2' -
+        D(i, 'J_b2') * D(i, 'O2') / data[i, ]$'Nb2' -
           (
-            GetConstant('A(b2)') +
-              GetDataValue(i, 'O2') * GetConstant('K(b2;O2)') * exp(-596 / GetDataValue(i, 'Tg')) +
-              GetDataValue(i, 'N2') * GetConstant('K(b2;N2)') +
-              GetDataValue(i, 'O3') * GetConstant('K(b2;O3)')
+            C('A(b2)') +
+              D(i, 'O2') * C('K(b2;O2)') * exp(-596 / D(i, 'Tg')) +
+              D(i, 'N2') * C('K(b2;N2)') +
+              D(i, 'O3') * C('K(b2;O3)')
           )
       )
     atomic.oxygen.denceties <- c(atomic.oxygen.denceties, new.dencity)
@@ -41,4 +44,4 @@ for (i in 1:nrow(data)) {
   }
 }
 colnames(result) <- c('height', 'size', 'mean', 'sd', 'variation', 'fraction')
-write.csv(result, file="science//result.csv")
+write.csv(result, file="science/O(3P)_from_O2(b,2)/results/result.csv")
--- O3_from_b1/O3_from_b1_retrieval.R
+++ O3_from_b1/O3_from_b1_retrieval.R
@@ -1,16 +1,19 @@
-data <- read.csv('science/data.csv', sep=';', check.names=F)
-constants <- read.csv('science//constants.csv', sep=';', check.names=F)
+data <- read.csv('science/O3_from_b1/data.csv', sep=';', check.names=F)
+constants <- read.csv('science/O3_from_b1/constants.csv', sep=';', check.names=F)
 
 GetValueWithError <- function(value, error) {
   value * (1 + rnorm(1, sd=error/2))
 }
 
-GetConstant <- function(name) {
+C <- function(name) {
   constant <- constants[, name]
-  GetValueWithError(constant[1], constant[1])
+  error = if (is.na(constant[2])) 0 else constant[2]
+  GetValueWithError(constant[1], error)
 }
 
-GetDataValue <- function(i, name) {
+D <- function(i, name) {
+  if (is.null(data[i, name]))
+    stop(paste('no parameter with name', name))
   GetValueWithError(data[i, name], constants[2, 'Rest'])  
 }
 
@@ -20,25 +23,42 @@ for (i in 1:nrow(data)) {
   atomic.oxygen.denceties <- numeric()
   for (j in 1:MAX) {
     new.dencity <- 
-      (1 / GetConstant('K(b2;O)')) * 
+    (
       (
-        GetDataValue(i, 'J_b2') * GetDataValue(i, 'O2') / data[i, ]$'Nb2' -
-          (
-            GetConstant('A(b2)') +
-              GetDataValue(i, 'O2') * GetConstant('K(b2;O2)') * exp(-596 / GetDataValue(i, 'Tg')) +
-              GetDataValue(i, 'N2') * GetConstant('K(b2;N2)') +
-              GetDataValue(i, 'O3') * GetConstant('K(b2;O3)')
-          )
+        data[i, ]$'O2(b,1)' * 
+        (
+          C('A(b1)') + C('K(b1;O2)') * D(i, 'O2') * exp(-312 / D(i, 'Tg')) +
+          C('K(b1;N2)') * D(i, 'N2') + C('K(b1;O)') * D(i, 'O(3P)')
+        ) -
+        D(i, 'O2') * D(i, 'J_b1')
+      ) * 
+      (
+        C('A(1D)') + D(i, 'O2') * C('K(1D;O2)') * exp(67/D(i, 'Tg')) +
+        D(i, 'N2') * C('K(1D;N2)') * exp(107 / D(i, 'Tg'))
+      ) /
+      (
+        D(i, 'Hartley') * C('F(D)') * C('K(1D;O2)') *
+        exp(67 / D(i, 'Tg')) * D(i, 'O2') * C('F(b1)')
       )
+    ) -
+    (
+      D(i, 'O2') * 
+      (
+        D(i, 'S.-R. C.') + D(i, 'LyA') * C('F(alpha)')
+      ) /
+      (
+        D(i, 'Hartley') * C('F(D)')
+      )
+    )
     atomic.oxygen.denceties <- c(atomic.oxygen.denceties, new.dencity)
     if ((j <= 200 && j %% 20 == 0) || (j > 200 && j %% 50 == 0)) {
       m <- mean(atomic.oxygen.denceties)
       s <- sd(atomic.oxygen.denceties)
       v <- s / m
-      r <- m / data[i, ]$Etalon
+      r <- m / data[i, ]$O3
       result <- rbind(result, c(data[i, ]$'z, km', j, m, s, v, r))
     }
   }
 }
 colnames(result) <- c('height', 'size', 'mean', 'sd', 'variation', 'fraction')
-write.csv(result, file="science//result.csv")
+write.csv(result, file="science/O3_from_b1/results/result.csv")
